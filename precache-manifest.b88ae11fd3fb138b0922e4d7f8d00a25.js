b91d3d42f589006ff2e2d0dc7238ebba5df5c227
Alex Shen <shen.alexander@gmail.com>
2020-05-27 13:41:05 -0400

    Updates

---
+++ precache-manifest.b88ae11fd3fb138b0922e4d7f8d00a25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a280f614af6e18f297effed2fc0f30",
+    "url": "/AdminPage/index.html"
+  },
+  {
+    "revision": "6933a3af9cc7a659c52f",
+    "url": "/AdminPage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "59b7003b5bb370beaa85",
+    "url": "/AdminPage/static/js/2.5a5a63b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AdminPage/static/js/2.5a5a63b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6933a3af9cc7a659c52f",
+    "url": "/AdminPage/static/js/main.da4500e9.chunk.js"
+  },
+  {
+    "revision": "1f07c661c3120fd72ada",
+    "url": "/AdminPage/static/js/runtime-main.5901734e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AdminPage/static/media/logo.5d5d9eef.svg"
+  }
+]);
